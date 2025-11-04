e878623634afdd75d02daafd2e2fb0c7bf11b73b
Makgomo-77 <102011950+Makgomo-77@users.noreply.github.com>
2025-11-04 21:49:18 +0200

    Create Footer.jsx

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <footer className="bg-dark text-white text-center py-3">
+      <p className="mb-0">&copy; {new Date().getFullYear()} Makgomo Shayi | All Rights Reserved</p>
+    </footer>
+  );
+}
